afaf21a011290f75e226a875d00b42d0b6edfdb8
osbornb <osbornb@bit-sys.com>
2015-03-24 11:03:06 -0600

    wkb library

---
+++ src/main/java/mil/nga/giat/wkb/util/WkbException.java
@@ -0,0 +1,50 @@
+package mil.nga.giat.wkb.util;
+
+/**
+ * Well-Known Binary exception
+ * 
+ * @author osbornb
+ */
+public class WkbException extends RuntimeException {
+
+	/**
+	 * Serial version id
+	 */
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * Constructor
+	 */
+	public WkbException() {
+		super();
+	}
+
+	/**
+	 * Constructor
+	 * 
+	 * @param message
+	 */
+	public WkbException(String message) {
+		super(message);
+	}
+
+	/**
+	 * Constructor
+	 * 
+	 * @param message
+	 * @param throwable
+	 */
+	public WkbException(String message, Throwable throwable) {
+		super(message, throwable);
+	}
+
+	/**
+	 * Constructor
+	 * 
+	 * @param throwable
+	 */
+	public WkbException(Throwable throwable) {
+		super(throwable);
+	}
+
+}
