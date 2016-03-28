0ea0fd7a08cb99dcd242b03f9063f574950e785b
Brian Osborn <bosborn@users.noreply.github.com>
2016-03-28 15:35:50 -0600

    Creation of JSON utility without functionality

---
+++ src/main/java/mil/nga/wkb/util/GeometryJSONCompatible.java
@@ -0,0 +1,24 @@
+package mil.nga.wkb.util;
+
+import mil.nga.wkb.geom.Geometry;
+
+/**
+ * JSON compatible object representation of a Geometry
+ * 
+ * @author osbornb
+ * @since 1.0.2
+ */
+public class GeometryJSONCompatible {
+
+	/**
+	 * Get a Geometry object that is JSON compatible
+	 * 
+	 * @param geometry
+	 *            geometry
+	 * @return geometry JSON object
+	 */
+	public static Object getJSONCompatibleGeometry(Geometry geometry) {
+		return null; // TODO
+	}
+
+}
