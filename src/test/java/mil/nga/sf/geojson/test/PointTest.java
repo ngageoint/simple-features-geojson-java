8507b97e36e04790f58779956fba5828de8bea3c
Jeff Yutzler <jyutzler@yahoo.com>
2017-05-10 14:50:01 -0400

    making Point test more reliable

--- src/test/java/mil/nga/sf/geojson/test/PointTest.java
+++ src/test/java/mil/nga/sf/geojson/test/PointTest.java
@@ -1,10 +1,13 @@
 package mil.nga.sf.geojson.test;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
+import junit.framework.TestCase;
 import mil.nga.sf.geojson.GeoJsonObject;
 import mil.nga.sf.geojson.GeoJsonObjectFactory;
+import mil.nga.sf.geojson.Geometry;
 import mil.nga.sf.geojson.Point;
 import mil.nga.sf.geojson.Position;
 
@@ -14,8 +17,6 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
 
-import static org.junit.Assert.*;
-
 public class PointTest {
 
 	private ObjectMapper mapper = new ObjectMapper();
@@ -23,17 +24,18 @@ public class PointTest {
 	@Test
 	public void itShouldSerializeASFPoint() throws Exception {
 		mil.nga.sf.Point point = new mil.nga.sf.Point(new mil.nga.sf.Position(100d, 10d));
-		String text = mapper.writeValueAsString(GeoJsonObjectFactory.createObject(point));
-		assertEquals("{\"type\":\"Point\",\"coordinates\":[100.0,10.0]}",
-				text);
+		Geometry geometry = GeoJsonObjectFactory.createObject(point);
+		JsonNode pointFromPojo = mapper.valueToTree(geometry);
+		JsonNode pointFromString = mapper.readTree("{\"type\":\"Point\",\"coordinates\":[100.0,10.0]}");
+		TestCase.assertEquals(pointFromPojo, pointFromString);
 	}
 
 	@Test
 	public void itShouldDeserializeAPoint() throws Exception {
 		GeoJsonObject value = mapper
 				.readValue("{\"type\":\"Point\",\"coordinates\":[100.0,5.0]}", GeoJsonObject.class);
-		assertNotNull(value);
-		assertTrue(value instanceof Point);
+		TestCase.assertNotNull(value);
+		TestCase.assertTrue(value instanceof Point);
 		Point point = (Point)value;
 		TestUtils.assertPoint(100d, 5d, null, point);
 	}
@@ -49,8 +51,10 @@ public class PointTest {
 	@Test
 	public void itShouldSerializeAPointWithAltitude() throws Exception {
 		mil.nga.sf.Point point = new mil.nga.sf.Point(new mil.nga.sf.Position(100d, 10d, 256d));
-		assertEquals("{\"type\":\"Point\",\"coordinates\":[100.0,10.0,256.0]}",
-				mapper.writeValueAsString(GeoJsonObjectFactory.createObject(point)));
+		Geometry geometry = GeoJsonObjectFactory.createObject(point);
+		JsonNode pointFromPojo = mapper.valueToTree(geometry);
+		JsonNode pointFromString = mapper.readTree("{\"type\":\"Point\",\"coordinates\":[100.0,10.0,256.0]}");
+		TestCase.assertEquals(pointFromPojo, pointFromString);
 	}
 
 	@Test
@@ -62,10 +66,12 @@ public class PointTest {
 	}
 
 	@Test
-	public void itShouldSerializeAPointWithAdditionalAttributes() throws JsonProcessingException {
+	public void itShouldSerializeAPointWithAdditionalAttributes() throws IOException {
 		Position position = new Position(100d, 0d, 256d, 345d, 678d);
-		Point point = new Point(position);
-		assertEquals("{\"type\":\"Point\",\"coordinates\":[100.0,0.0,256.0,345.0,678.0]}",
-				mapper.writeValueAsString(point));
+		mil.nga.sf.Point point = new mil.nga.sf.Point(position);
+		Geometry geometry = GeoJsonObjectFactory.createObject(point);
+		JsonNode pointFromPojo = mapper.valueToTree(geometry);
+		JsonNode pointFromString = mapper.readTree("{\"type\":\"Point\",\"coordinates\":[100.0,0.0,256.0,345.0,678.0]}");
+		TestCase.assertEquals(pointFromPojo, pointFromString);
 	}
 }
