cd10009047bab7594ad0f97871c4b9ff7fa4a69a
Brian Osborn <bosborn@users.noreply.github.com>
2018-04-10 12:02:11 -0600

    refactory of geometry hierarchy to support geometry collections

---
+++ src/main/java/mil/nga/sf/geojson/Geometry.java
@@ -0,0 +1,31 @@
+package mil.nga.sf.geojson;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonSubTypes;
+import com.fasterxml.jackson.annotation.JsonSubTypes.Type;
+
+/**
+ * Geometry
+ * 
+ * @author yutzlejp
+ */
+@JsonSubTypes({ @Type(Polygon.class), @Type(MultiPolygon.class),
+		@Type(Point.class), @Type(MultiPoint.class),
+		@Type(MultiLineString.class), @Type(LineString.class),
+		@Type(GeometryCollection.class) })
+@JsonIgnoreProperties({ "geometry" })
+public abstract class Geometry extends GeoJsonObject {
+
+	/**
+	 * Serialization Version number
+	 */
+	private static final long serialVersionUID = -937799573407640901L;
+
+	/**
+	 * Get the simple geometry
+	 * 
+	 * @return simple geometry
+	 */
+	public abstract mil.nga.sf.Geometry getGeometry();
+
+}
